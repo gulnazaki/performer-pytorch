4346b648ed6a2aaee28a2e4d977e2a11a0fb5227
gulnazaki <tom96meli@gmail.com>
2021-02-05 23:27:59 +0200

    original transformer sublayers - works for sequential

--- performer_pytorch/reversible.py
+++ performer_pytorch/reversible.py
@@ -6,14 +6,14 @@ from torch.utils.checkpoint import get_device_states, set_device_states
 
 # for routing arguments into the functions of the reversible layer
 def route_args(router, args, depth):
-    routed_args = [(dict(), dict()) for _ in range(depth)]
+    routed_args = [(dict(),)*len(list(router.values())[0]) for _ in range(depth)]
     matched_keys = [key for key in args.keys() if key in router]
 
     for key in matched_keys:
         val = args[key]
-        for depth, ((f_args, g_args), routes) in enumerate(zip(routed_args, router[key])):
-            new_f_args, new_g_args = map(lambda route: ({key: val} if route else {}), routes)
-            routed_args[depth] = ({**f_args, **new_f_args}, {**g_args, **new_g_args})
+        for depth, (fun_args, routes) in enumerate(zip(routed_args, router[key])):
+            new_fun_args = map(lambda route: ({key: val} if route else {}), routes)
+            routed_args[depth] = tuple({**f_args, **new_f_args} for f_args, new_f_args in zip(fun_args, new_fun_args))
     return routed_args
 
 # following example for saving and setting rng here https://pytorch.org/docs/stable/_modules/torch/utils/checkpoint.html
@@ -134,9 +134,9 @@ class SequentialSequence(nn.Module):
         args = route_args(self.args_route, kwargs, len(self.layers))
         layers_and_args = list(zip(self.layers, args))
 
-        for (f, g), (f_args, g_args) in layers_and_args:
-            x = x + f(x, **f_args)
-            x = x + g(x, **g_args)
+        for fun, fun_args in layers_and_args:
+            for f, f_args in zip(fun, fun_args):
+                x = x + f(x, **f_args)
         return x
 
 class ReversibleSequence(nn.Module):
